3c33c6bbba8d1c2b4d5df7216ee3f2359ff56028
StevenLance <44912347+stevenjlance@users.noreply.github.com>
2021-10-16 15:04:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,109 @@
+// DO NOW: Complete the problems below using the array and object provided
+const highestGrossingMovies = ["Avatar", "Avengers: Endgame",	"Titanic","Star Wars: Episode VII - The Force Awakens", "Avengers: Infinity War",	"Jurassic World",	"The Lion King", "The Avengers", "Furious 7", "Frozen II"]
+
+//a. Print out the 7th highest grossing movie of all time (The Lion King)
+
+
+// b. Add "Black Panther", the the 12th highest grossing movie of all time to the list. Make sure it is on teh end.
+
+
+// c. Remove Avatar from the list
+
+
+
+
+
+const superheroes = {
+  "steven": "Wonder Woman",
+  "taylor": "Jessica Jones",
+  "danny": "Ms. Marvel",
+  "gabe": "Supergirl",
+  "sara": "The Hulk",
+  "shana": "Black Panther"
+} 
+// d. Print out taylor's favorite super hero
+
+
+// e. Add your favorite superhero to the object
+
+
+// f. Delete steven's favorite superhero
+
+
+
+// =============== FOR LOOPS ====================
+// 1. CODE-ALONG: Log out every number from 1-20
+
+
+// 2. Log out every number from 50-100
+
+
+// 3. Log out every even number up to 100
+
+
+// 4. Log out every multiple of 2 and every multiple of 3 up to 100
+
+
+// 5. Log out the sum of every number under 1000
+
+
+// 6. Log out the first 10 digits of the fibbonacci series.
+
+
+
+// ======= TRAVERSING ARRAYS WITH FOR LOOPS ===========
+const names = ["Yadira", "Kadiatou", "Isaiah", "Fancisco", "Darius"]
+const nicknames = ["Deary", "Kadi-B", "Izzy", "Paco", "DRock"]
+const ages = [22, 28, 27, 33, 29]
+const favAnimals = ["Cat", "Ferret", "Dog", "Sloth", "Zebra"]
+const birthLocations = ["Queens", "Bronx", "Manhattan", "Bronx", "Brooklyn"]
+
+// 7. CODE-ALONG: Print our all of the names in the list
+
+
+
+// 8. CODE-ALONG: Print out "________ was born in _________" for each individual
+
+
+
+
+// 9. Print out "_________ goes by the name _______ and is ________ years old"
+
+
+
+/* 10. Print out info for each person. For example, it may look like:
+Name: Yadira
+Nickname: "Deary"
+Age: 22
+Favorite Animal: Cat
+Birth Location: Queens
+*/ 
+
+
+
+// =========== NESTED DATA & FOR LOOPS =============
+// 11. The current data would make sense much more as an object rather than individual arrays. Use a for loop to update the people array with objects for each person.
+
+const people = []
+
+
+
+// 12. Using the new people array you created and a for loop, print out "_________'s nickname is _______" for each person.
+
+
+
+
+// 13. Using the new people array you created and a for loop, print out "_________'s favorite animal is _______" for each person.
+
+
+
+/*14. STRETCH: Can you make each object an HTML list? For example, one may look like:
+<ul>
+<li>Name: Yadira</li>
+<li>Nickname: "Deary"</li>
+<li>Age: 22</li>
+<li>Favorite Animal: Cat</li>
+<li>Birth Location: Queens</li>
+</ul>
+*/
+
